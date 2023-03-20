8bd75735cee2aeebc391a300349f19e71fa20ec1
wilberquito <wbq.software@gmail.com>
2023-03-20 21:14:19 +0100

    adds pytorch engine to train models

--- modular/datasets.py
+++ modular/datasets.py
@@ -31,13 +31,13 @@ def get_transforms(img_size:int):
 
     return train_transform, valid_transform
 
-def get_dataloader(folder_root, transformer, batchsize=32, suffle=False):
+def get_dataloader(folder_root, transformer, batch_size=32, suffle=False):
 
     dataset = torchvision.datasets.ImageFolder(root=folder_root,transform=transformer)
 
     # training data loaders
     dataloader = DataLoader(dataset,
-                            batch_size=batchsize,
+                            batch_size=batch_size,
                             shuffle=suffle)
 
     return dataloader
