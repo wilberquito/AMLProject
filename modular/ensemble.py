d95e0e3855eb75e4030aa14546020516c99708e2
wilberquito <wbq.software@gmail.com>
2023-04-11 12:28:52 +0200

    Fix ensemble function

--- modular/ensemble.py
+++ modular/ensemble.py
@@ -1,32 +1,56 @@
+"""
+Script thought to ensemble predictions from differents models.
+It requires an standard form of the predictions. All predictions
+must have the same number of columns and the predictions ordered
+by index. The indexes of all predictions must represent the name of
+the image.
+"""
+
+
 import pandas as pd
 from glob import glob
 import os
 import numpy as np
-from typing import cast
 
 
 def ensemble(parent_dir: str,
              output_name: str):
+    """
+    Ensemble fun, all predictions are weighted the same
+    """
 
-    subs = []
+    all_csv = []
     for csv in sorted(glob(os.path.join(parent_dir, '*.csv'))):
-        subs.append(cast(pd.DataFrame, pd.read_csv(csv)))
+        all_csv.append(pd.read_csv(csv, index_col=0))
 
-    n, _ = subs[0].shape
-    n_csv = len(subs)
+    # Computes how many all_csv has the first csv.
+    # All csv has to have the same name of samples...
+    n, _ = all_csv[0].shape
+    # Computes how many different csv are
+    n_csv = len(all_csv)
+    # Weights for each csv
     wts = [1/n_csv]*n_csv
+    # Get the name of all images
+    names = list(all_csv[0].index)
+    # Here I save the labels predicted by the ensemble
     labels = []
-    names = subs[0].names
 
+    # Per each sample, I compute the average weighted,
+    # of the different predictions
     for i in range(n):
-        logits = np.sum([sub.iloc[i] * wts[i] for sub in subs])
-        labels.append(np.argmax(logits))
+        all_preds = [csv.iloc[i].values * wts[j] for j, csv in enumerate(all_csv)]
+        logits = np.sum(all_preds, axis=0)
+        labels.append(np.argmax(logits, axis=0))
 
     df = pd.DataFrame({
-        'names': names,
+        'name': names,
         'class': labels
     })
-    df.sort_values(by='names',
-                   ascending=False,
+    df.sort_values(by='name',
+                   ascending=True,
                    inplace=True)
     df.to_csv(output_name, index=False)
+
+
+if __name__ == '__main__':
+    ensemble('./predictions', 'ensemble.csv')
